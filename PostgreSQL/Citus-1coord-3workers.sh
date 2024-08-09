3fa780fc96f30b04cbdf60f70e29a7dd91129fd5
グリアンドロ <guriandoro@gmail.com>
2024-08-09 19:43:42 -0300

    Update Citus-1coord-3workers.sh

--- PostgreSQL/Citus-1coord-3workers.sh
+++ PostgreSQL/Citus-1coord-3workers.sh
@@ -35,6 +35,7 @@ cd
 bash /run_citus.sh
 EOF
 
+# Run setup steps on all nodes
 for node in node0 node1 node2 node3; do
   echo; echo "## Setup for" $node
   docker cp /tmp/run_alters.sql citus-$(whoami|tr '.' '-')-$node:run_alters.sql
@@ -51,3 +52,40 @@ echo "SELECT citus_add_node('node2', 5432);"
 echo "SELECT citus_add_node('node3', 5432);"
 echo "SELECT citus_get_active_worker_nodes();"
 } | anydbver --namespace=citus exec node0 -- sudo -u postgres psql -Upostgres db01
+
+# Some misc tests
+# Connect to coordinator
+anydbver --namespace=citus exec node0 -- sudo -u postgres psql -Upostgres db01
+
+# Optionally enable logging of commands
+#ALTER SYSTEM SET citus.log_remote_commands=on;
+#SELECT pg_reload_conf();
+
+# Get active workers
+SELECT * from master_get_active_worker_nodes() ORDER BY 1;
+
+# Create distributed table
+CREATE TABLE myevents (
+    device_id bigint,
+    event_id bigserial,
+    event_time timestamptz default now(),
+    data jsonb not null,
+    primary key (device_id, event_id)
+);
+SELECT create_distributed_table('myevents', 'device_id');
+
+# Get distributed tables
+SELECT * FROM citus_tables;
+
+# Get shards
+SELECT shard_name, citus_table_type, nodename, shard_size 
+FROM citus_shards;
+
+# Insert data into table
+INSERT INTO myevents (device_id, data) 
+  SELECT s % 100, ('{"measurement":'||random()||'}')::jsonb
+  FROM generate_series(1,1000000) s;
+
+# We can use the "get" queries above to see how stats changed for table and shard sizes
+SELECT * FROM citus_tables;
+SELECT * FROM citus_shards;
