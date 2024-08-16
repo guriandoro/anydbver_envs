c4c2db9f3cdb41319b10cf56d498f55d70241a07
グリアンドロ <guriandoro@gmail.com>
2024-08-16 19:49:16 -0300

    Update Citus-1coord-3workers.sh

--- PostgreSQL/Citus-1coord-3workers.sh
+++ PostgreSQL/Citus-1coord-3workers.sh
@@ -4,44 +4,34 @@ node1 pg:latest \
 node2 pg:latest \
 node3 pg:latest 
 
-# Check network address, if needed, to change pg_hba.conf rule
-anydbver --namespace=citus exec node0 -- ip a
+# Get node0's IP address to automatically edit pg_hba.conf rule below
+ANYNET=`anydbver --namespace=citus exec node0 -- ip a | grep "inet " | grep -v "127.0.0.1" | awk '{print $2}' | cut -d '/' -f1`
+echo "IP address for node0 is" $ANYNET
 
-cat <<EOF >/tmp/run_alters.sql
- alter system set wal_level = 'logical';
- alter system set listen_addresses = '*';
- alter system set ssl = 'on';
- show shared_preload_libraries;
- alter system set shared_preload_libraries = 'citus';
- create database db01;
-EOF
-
-cat <<EOF >/tmp/run_citus.sh
- sudo -u postgres psql db01 -c "create extension citus";
-EOF
-
-cat <<EOF >/tmp/run_setup.sh
+# Run setup steps on all nodes
+for NODE in node0 node1 node2 node3; do
+  echo; echo "## Setup for" $NODE
+  anydbver --namespace=citus exec $NODE <<EOF
 echo -n "Running setup in: "
 hostname
-yum -y install citus_16.x86_64
-sudo -u postgres psql < run_alters.sql
-sed -i -e 's/local   all             all/hostssl all all 192.168.0.0\/16 trust\nlocal all all/' /var/lib/pgsql/16/data/pg_hba.conf
+yum -q -y install citus_16.x86_64
+{
+echo "alter system set wal_level = 'logical';"
+echo "alter system set listen_addresses = '*';"
+echo "alter system set ssl = 'on';"
+echo "show shared_preload_libraries;"
+echo "alter system set shared_preload_libraries = 'citus';"
+echo "create database db01;"
+} | sudo -u postgres psql
+echo "running sed"
+sed -i -e '0,/^host / s/^host /hostssl all all ${ANYNET}\/24 trust\nhost /' /var/lib/pgsql/16/data/pg_hba.conf
 cd /var/lib/pgsql/16/data/
 openssl req -nodes -new -x509 -keyout server.key -out server.crt -subj '/C=US/L=NYC/O=Percona/CN=postgres'
 chmod 400 server.{crt,key}
 chown postgres:postgres server.{crt,key}
 systemctl restart postgresql-16
-cd
-bash /run_citus.sh
+sudo -u postgres psql db01 -c "create extension citus";
 EOF
-
-# Run setup steps on all nodes
-for node in node0 node1 node2 node3; do
-  echo; echo "## Setup for" $node
-  docker cp /tmp/run_alters.sql citus-$(whoami|tr '.' '-')-$node:run_alters.sql
-  docker cp /tmp/run_citus.sh citus-$(whoami|tr '.' '-')-$node:run_citus.sh
-  docker cp /tmp/run_setup.sh citus-$(whoami|tr '.' '-')-$node:run_setup.sh
-  anydbver --namespace=citus exec $node -- bash run_setup.sh
 done
 
 # On coordinator node only
